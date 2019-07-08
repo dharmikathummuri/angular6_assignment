f497c69ef201527503b5fb7e044ddfec28b4fe8c
dharmika <dharmikasharma@gmail.com>
2019-07-08 11:02:59 -0400

    create list-posts and list-user components and add routing for that

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ListUsersComponent } from "./users/list-users.component";
+import { ListPostsComponent } from "./users/list-posts.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "list-users",
+    component: ListUsersComponent
+  },
+  {
+    path: "list-posts",
+    component: ListPostsComponent
+  },
+  { path: "", redirectTo: "list-users", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListUsersComponent } from './users/list-users.component';
+import { ListPostsComponent } from './users/list-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListUsersComponent,
+    ListPostsComponent
   ],
   imports: [
     BrowserModule,
