a32008aabdfeecc3d28276d3a68a7729dd58e951
dharmika <dharmikasharma@gmail.com>
2019-07-09 20:46:10 -0400

    create form for post data and submit a post request , validate data and display error and success messages accordingly.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ListUsersComponent } from "./users/list-users.component";
@@ -9,16 +9,23 @@ import { HttpClientModule } from "@angular/common/http";
 import { UserService } from "src/services/user.service";
 import { PostService } from "src/services/post.service";
 import { NgxPaginationModule } from "ngx-pagination";
-import { DisplayUserComponent } from './users/display-user.component';
-import { DisplayPostComponent } from './posts/display-post.component';
+import { DisplayUserComponent } from "./users/display-user.component";
+import { DisplayPostComponent } from "./posts/display-post.component";
 
 @NgModule({
-  declarations: [AppComponent, ListUsersComponent, ListPostsComponent, DisplayUserComponent, DisplayPostComponent],
+  declarations: [
+    AppComponent,
+    ListUsersComponent,
+    ListPostsComponent,
+    DisplayUserComponent,
+    DisplayPostComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    FormsModule
   ],
   providers: [UserService, PostService],
   bootstrap: [AppComponent]
--- src/app/posts/list-posts.component.ts
+++ src/app/posts/list-posts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { PostService } from "src/services/post.service";
 import { Router } from "@angular/router";
+import { IPost } from "../interfaces/post";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-list-posts",
@@ -9,8 +11,19 @@ import { Router } from "@angular/router";
 })
 export class ListPostsComponent implements OnInit {
   public posts = [];
+  public postData = { title: "", body: "" };
+  displayForm = false;
+  errorMessage = [];
+  errors = false;
+  success = false;
+  currentid: number;
+  disableButton = false;
   config: any;
-  constructor(private _postService: PostService, private _router: Router) {
+  constructor(
+    private _postService: PostService,
+    private _router: Router,
+    private http: HttpClient
+  ) {
     this.config = {
       itemsPerPage: 10,
       currentPage: 1,
@@ -21,10 +34,67 @@ export class ListPostsComponent implements OnInit {
   ngOnInit() {
     this._postService.getPost().subscribe(data => (this.posts = data));
   }
+  clearFormData() {
+    this.postData = { title: "", body: "" };
+    this.errorMessage = [];
+  }
   pageChanged(event) {
     this.config.currentPage = event;
   }
   onClick(id) {
     this._router.navigateByUrl(`display-post/${id}`);
+  }
+  showForm() {
+    this.displayForm = this.displayForm == true ? false : true;
+  }
+  validateInput(data) {
+    let letters = /^[0-9a-zA-Z]+$/;
+    let condition = true;
+    if (data.title.length > 24 || data.body.length > 50) {
+      this.errorMessage.push(
+        "Please make sure that title shoul be less than 24 characters and post should be less than 50 characters!!"
+      );
+      condition = false;
+    }
+    if (!data.title.match(letters)) {
+      this.errorMessage.push(
+        "only enter alpha numeric values in the title box!!"
+      );
+      condition = false;
+    }
+    if (data.title.length == 0 || data.body.length == 0) {
+      this.errorMessage.push("Do not submit until you will the form!!");
+      condition = false;
+    }
+    this.errorMessage = this.errorMessage.slice(0, 3);
+    return condition;
+  }
+  savePost(e) {
+    e.preventDefault();
+    const formValidated = this.validateInput(this.postData);
+    if (formValidated) {
+      this.http
+        .post("https://jsonplaceholder.typicode.com/posts", {
+          method: "POST",
+          body: JSON.stringify({
+            title: this.postData.title,
+            body: this.postData.body,
+            userId: 1
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        })
+        .subscribe(response => {
+          this.success = true;
+          this.errors = false;
+          this.currentid = response["id"];
+          this.clearFormData();
+        });
+    } else {
+      this.disableButton = true;
+      this.success = false;
+      this.errors = true;
+    }
   }
 }
